e59a9f81cee0f09ca2cc871c97a17b2c11079914
Natalia Zychowicz <115657550+LynxakaNat@users.noreply.github.com>
2023-05-20 13:13:56 +0200

    Dev (#2)
    * API connection
    
    * Bot init added
    
    * ApiConnector class
    
    Added the token refreshment, tidied up the class and commented the code

---
+++ ApiConnector.py
@@ -0,0 +1,78 @@
+from datetime import datetime, timedelta
+
+import requests
+
+
+# Here we are setting the API
+class ApiConnector(object):
+    baseURL = "https://www.warcraftlogs.com:443/api/v2/client"
+    oauth_auth_uri = "https://www.warcraftlogs.com/oauth/authorize"
+    token_url = "https://www.warcraftlogs.com/oauth/token"
+    current_oauth_token = None
+    current_oauth_token_expiry = None
+    refresh_time = 86400
+
+    def __init__(self, clientid, secret_key):
+        """
+
+        :param clientid: the WCL client ID created when creating an app for WCL
+        :param secret_key: the secret key DO NOT SHARE THE KEY
+        """
+
+        self.client_id = clientid
+        self.secret_key = secret_key
+        # Create session
+
+        self.CreateOAuthSession()
+
+    def CreateOAuthSession(self):
+        """
+        Creates a new OAuth session for WCL
+        :return: this function does not return anything
+        """
+        # Me: hey server my name is Lynx I want to get access
+        # Server : hey this is the info now gtfo
+        # This allows us to let requests.post take care of the OAuth
+        auth = requests.auth.HTTPBasicAuth(self.client_id, self.secret_key)
+        data = {"grant_type": "client_credentials"}
+        response = requests.post(self.token_url, data=data, auth=(self.client_id, self.secret_key))
+        print(response.status_code)
+        resp = response.json()
+        # print(resp)
+        print(response.headers)
+        self.current_oauth_token = resp["access_token"]
+        self.current_oauth_token_expiry = (datetime.now() + timedelta(seconds=resp["expires_in"]))
+
+    def RenewToken(self):
+        """
+        This function checks if the access token has expired or not. If it has we create a new oauth session
+        We also refresh if the token is valid for less than one day
+        :return: this function does not return anything
+        """
+
+        if self.current_oauth_token is None:
+            self.CreateOAuthSession()
+        elif self.current_oauth_token_expiry is None:
+            self.CreateOAuthSession()
+        elif (self.current_oauth_token_expiry - datetime.now()).total_seconds() < self.refresh_time:
+            self.CreateOAuthSession()
+        else:
+            return
+
+    def Request(self, query):
+        """
+
+        :param query: This is what we want to request from Warcraft Logs
+        :return: Returns the response from the WCL API for the query we gave it
+
+        """
+        self.RenewToken()  # we always want to check if the token has not expired
+        url = self.baseURL
+        headers = {
+            "authorization": "Bearer {}".format(self.current_oauth_token),
+            "accept": "application/json"
+        }
+        response = requests.get(url, json={'query': query}, headers=headers)
+        # print(response.url + " " + str(response.status_code))
+        response.raise_for_status()
+        return response.json()
